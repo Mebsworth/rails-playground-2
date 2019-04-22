602d44b840b283ba634e03fc591ea4192e727976
Maya Ebsworth <maya.ebsworth@gmail.com>
2019-04-22 14:46:28 -0700

    add validation and testing to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ApplicationRecord
+
+# /           start of regex
+# \A          start of string
+# [\w+\-.]+   at least one word char, plus, hyphen, or dot.
+# @           literal @ sign
+# [a-z\d\-.]+ at least one letter, digit, hyphen, or dot.
+# \.          literal dot
+# [a-z]+      at least one letter
+# \z	        match end of a string
+# /	          end of regex
+# i	          case-insensitive
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   has_many :microposts
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end
